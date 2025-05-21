d6d05cae64d27883d9a8a1dc3de3255dc4f4576c
Networksentinel <network.sentinel@protonmail.com>
2025-05-21 07:46:57 +0200

    docs: added vite.config.js and edited the base path for GitHub deployment

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+console.log("vite.config.js is loaded!");
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Frontend-Mentor-Challenge---Testimonials-grid-section/',
+});
